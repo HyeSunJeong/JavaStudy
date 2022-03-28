66483d6a8c4d831f7e65d711a38127dcec2784e8
HyeSunJeong <zaqxswcde88@naver.com>
2022-03-28 17:15:56 +0900

    integer 567, 백, 십, 일자리 분리

--- HelloWolrd/src/App.java
+++ HelloWolrd/src/App.java
@@ -1,5 +1,9 @@
+//정수 567을 백의자리, 십의자리, 일의자리로 분리하는 프로그램
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        int a = 567;
+        System.out.println("정수 567의 백의자리는 : " +a/100);
+        System.out.println("정수 567의 십의자리는 : " +(a%100)/10);
+        System.out.println("정수 567의 일의자리는 : " +(a%100)%10);
     }
 }
